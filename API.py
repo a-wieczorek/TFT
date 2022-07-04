b1a426b114654cd7e567860d7b9e82c6299d1e98
a-wieczorek <adiw98@o2.pl>
2022-07-04 11:35:50 +0200

    Swap lang

--- API.py
+++ API.py
@@ -1,9 +1,12 @@
 import json
 import pickle
 from dataclasses import dataclass
+from enum import Enum
+
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 
 @dataclass
@@ -14,40 +17,95 @@ class Champion:
 
 
 app = FastAPI()
-x = open("data_pickle.txt", "rb")
-champions = pickle.load(x)
-x.close()
+p = open("data_pickle.txt", "rb")
+champions = pickle.load(p)
+p.close()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["null"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+translator = {
+    "Shimmerscale": "Migocząca Łuska",
+    "Warrior": "Wojownik",
+    "Jade": "Nefrytowy",
+    "Evoker": "Przywołujący",
+    "Legend": "Legenda",
+    "Tempest": "Nawałnica",
+    "Dragon": "Smok",
+    "Swiftshot": "Szybkostrzelny",
+    "Astral" : "Gwiezdny",
+    "Dragonmancer": "Wysłannik Smoków",
+    "Mage": "Mag",
+    "Mirage": "Miraż",
+    "Revel": "Festynowy",
+    "Bruiser": "Osiłek",
+    "Guild": "Z Gildii",
+    "Mystic": "Mistyk",
+    "Bard": "Bard",
+    "Scalescorn": "Łuskobójcy",
+    "Guardian": "Strażnik",
+    "Cannoneer": "Kanonier",
+    "Assassin": "Zabójca",
+    "Whispers": "Szepty",
+    "Shapeshifter": "Zmiennokształtny",
+    "Ragewing": "Skrzydło Gniewu",
+    "Cavalier": "Kawalerzysta",
+    "Trainer": "Trener",
+    "Starcaller": "Zaklinacz Gwiazd",
+    "Spellthief": "Złodziej Czarów",
+}
 
 
 def get_with_all_traits(traits: List[str]) -> List[str]:
     result: List[str] = []
     for c in champions:
-        if all(x in c.traits for x in traits):
+        if all(x.lower() in c.traits for x in traits):
             result.append(c.name)
     return result
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request, traits=None):
+    allTraits = []
     myDict = {}
+    miniDicts = []
     if traits is None:
         traits = []
     else:
-        traits = traits.split(',')
+        traits = traits[:-1].split(',')
     names = get_with_all_traits(traits)
     for i in names:
         for k in champions:
+            allTraits = allTraits + [trait.capitalize() for trait in k.traits]
             miniDict = {}
             if i == k.name:
-                miniDict['traits'] = k.traits
+                capTraits = [x.capitalize() for x in k.traits]
+                miniDict['name'] = k.name
+                miniDict['traits'] = capTraits
                 miniDict['price'] = k.price
                 miniDict['iconurl'] = f'https://raw.githubusercontent.com/a-wieczorek/TFT/main/IconsEdited/{i}.jpg'
-                myDict[i] = miniDict
-
+                miniDicts.append(miniDict)
+    myDict['champions'] = miniDicts
+    myDict['traitListEN'] = sorted(set(allTraits))
+    myDict['traitListPL'] = sorted([[translator[trait], trait] for trait in allTraits])
     return json.dumps(myDict)
 
 
-
-
+@app.get("/traitlists", response_class=HTMLResponse)
+async def returnLists(request: Request):
+    allTraits = []
+    result = {}
+    for k in champions:
+        allTraits = allTraits + [trait.capitalize() for trait in k.traits]
+    allTraits = set(allTraits)
+    result['traitListEN'] = sorted(allTraits)
+    result['traitListPL'] = sorted([[translator[trait], trait] for trait in allTraits])
+    return json.dumps(result)
 
 uvicorn.run(app, host='localhost', port=8000)
